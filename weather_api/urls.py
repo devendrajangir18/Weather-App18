d98fd15149c39d15ea9be38fe3c42a2fb67b5fbe
Deven <devenj185@gmail.com>
2023-12-13 23:25:09 +0530

    login feature added

--- weather_api/urls.py
+++ weather_api/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
+    path('weather_api/home.html', views.index, name="home"),
+    path('', views.loginUser, name="login"),
+    path('login', views.loginUser, name="login"),
     path("result", views.result, name="result"),
+    path('logout', views.logoutUser, name="logout"),
     # path('social_links', views.social_links),
 ]
--- weather_api/views.py
+++ weather_api/views.py
@@ -1,16 +1,46 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import logout, authenticate, login
 from weather_api.key import api_key
 import requests
 import math
 # from .models import Social
 
-# Create your views here.
 
 def index(request):
-    return render(request, "weather_api/home.html")
+    print(request.user)
+    if request.user.is_anonymous:
+        return redirect("/login")
+    return render(request, 'weather_api/home.html')
+
+# def home(request):
+    # return render(request, "weather_api/home.html")
 
 
+    
+def loginUser(request):
+    if request.method=="POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(username, password)
+        
+        # check if user has entered correct credentials
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            # A backend authenticated the credentials
+            login(request, user)
+            return redirect("/weather_api/home.html")
+        else:
+            print("here")
+            # No backend authenticated the credentials
+            return render(request, 'login.html')
+        
+    return render(request, 'login.html')
+    
 
+def logoutUser(request):
+    logout(request)
+    return redirect("/login")
 
 def result(request):
     if request.method == "POST":
